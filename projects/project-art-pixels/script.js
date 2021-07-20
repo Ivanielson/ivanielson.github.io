a552c85d01d8506d82a516af5c27b52db74608c4
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-19 22:58:08 -0300

    adicionando projeto art pixels no repositório

---
+++ projects/project-art-pixels/script.js
@@ -0,0 +1,147 @@
+// Adiciona as cores da paleta de cores;
+const colors = ['background: rgb(0, 0, 0)'];
+
+for (let index = 0; index < 3; index += 1) {
+  let color = '';
+  for (let count = 0; count < 3; count += 1) {
+    let generationNumber = Math.floor(Math.random() * 256);
+    if (count === 0) {
+      color += 'background: rgb(' + generationNumber.toString();
+    } else if (count === 2) {
+      color += ',' + generationNumber.toString() + ')';
+    } else {
+      color += ',' + generationNumber.toString();
+    }
+  }
+  colors.push(color);
+}
+
+// Criação da tabela #pixel-board dinâmicamente;
+function createTable(size) {
+  const tableSelect = document.querySelector('#pixel-board');
+  for (let index = 0; index < size; index += 1) {
+    const trCreate = document.createElement('tr');
+    tableSelect.appendChild(trCreate);
+    const contadorLine = index;
+    for (let count = 0; count < size; count += 1) {
+      const trSelect = document.querySelectorAll('#pixel-board tr');
+      const tdCreate = document.createElement('td');
+      tdCreate.setAttribute('class', 'pixel');
+      trSelect[contadorLine].appendChild(tdCreate);
+    }
+  }
+}
+createTable(5);
+
+// Remove Pixel borad
+function removeBoardPixel() {
+  const boardSelect = document.querySelectorAll('#pixel-board tr');
+  for (let index = 0; index < boardSelect.length; index += 1) {
+    boardSelect[index].parentNode.removeChild(boardSelect[index]);
+  }
+}
+
+const trChindren = document.querySelector('tr').children;
+
+for (let index = 0; index < trChindren.length; index += 1) {
+  trChindren[index].setAttribute('style', colors[index]);
+}
+
+const color = document.querySelector('.color');
+color.classList.add('selected');
+
+function pintaPixel() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', function (event) {
+      const colorSelect = document.querySelector('.selected').style.background;
+      event.target.setAttribute('style', 'background:' + colorSelect);
+    });
+}
+}
+pintaPixel();
+
+function selectedChange(event) {
+  const colorsSelected = document.querySelector('.selected');
+  colorsSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+const divColors = document.querySelectorAll('.color');
+
+for (let index = 0; index < divColors.length; index += 1) {
+  const div = divColors[index];
+  div.addEventListener('click', selectedChange);
+}
+
+const button = document.createElement('button');
+button.innerText = 'Limpar';
+button.setAttribute('id', 'clear-board');
+const selectDiv = document.querySelector('#button');
+selectDiv.appendChild(button);
+
+function pixelClear() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    const pixel = pixels[index];
+    pixel.removeAttribute('style');
+  }
+}
+
+const selectButton = document.querySelector('#clear-board');
+selectButton.addEventListener('click', pixelClear);
+
+// Cria input;
+const inputCreate = document.createElement('input');
+inputCreate.setAttribute('type', 'number');
+inputCreate.setAttribute('max', '50');
+inputCreate.setAttribute('min', '1');
+
+// inputCreate.setAttribute('value', 'min');
+inputCreate.setAttribute('id', 'board-size');
+const selectDivInput = document.querySelector('#input');
+selectDivInput.appendChild(inputCreate);
+
+// Função retorna valor do input válido;
+function changeSizeBoardPixel() {
+  const inputSelect = document.querySelector('#board-size');
+  let valueInput = inputSelect.value;
+  if (inputSelect.value.length === 0) {
+    alert('Board inválido!');
+  } else {
+    if (inputSelect.value < 5) {
+      valueInput = 5;
+    }
+    if (inputSelect.value > 50) {
+      valueInput = 50;
+    }
+  }
+  return valueInput;
+}
+
+// Cria butão do input;
+const createButton = document.createElement('button');
+createButton.setAttribute('id', 'generate-board');
+createButton.innerText = 'VQV';
+selectDivInput.appendChild(createButton);
+
+// function generetaionBorad() {
+//   const sizeBoard = changeSizeBoardPixel();
+//   if (sizeBoard !== undefined) {
+//     removeBoardPixel();
+//     createTable(sizeBoard);
+//   }
+// }
+
+function generetaionBorad() {
+  const sizeBoard = changeSizeBoardPixel();
+  if (sizeBoard !== undefined) {
+    removeBoardPixel();
+    createTable(sizeBoard);
+  }
+  pintaPixel();
+  selectButton.addEventListener('click', pixelClear);
+}
+
+const buttonPixel = document.querySelector('#generate-board');
+buttonPixel.addEventListener('click', generetaionBorad);
